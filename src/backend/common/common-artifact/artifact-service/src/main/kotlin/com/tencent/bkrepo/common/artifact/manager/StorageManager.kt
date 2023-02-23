08ffc79778aafdc7e7eae11a431d250fac827e49
yaoxuwan <yaoxuwan@tencent.com>
2023-02-23 18:26:46 +0800

    feat: 增加EdgePlus集群角色 #224

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/manager/StorageManager.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/manager/StorageManager.kt
@@ -30,6 +30,7 @@ package com.tencent.bkrepo.common.artifact.manager
 import com.tencent.bkrepo.common.api.constant.HttpStatus
 import com.tencent.bkrepo.common.api.exception.ErrorCodeException
 import com.tencent.bkrepo.common.api.message.CommonMessageCode
+import com.tencent.bkrepo.common.api.pojo.ClusterNodeType
 import com.tencent.bkrepo.common.artifact.api.ArtifactFile
 import com.tencent.bkrepo.common.artifact.cluster.FeignClientFactory
 import com.tencent.bkrepo.common.artifact.repository.context.ArtifactContextHolder
@@ -40,7 +41,6 @@ import com.tencent.bkrepo.common.artifact.stream.artifactStream
 import com.tencent.bkrepo.common.artifact.util.http.HttpRangeUtils.resolveRange
 import com.tencent.bkrepo.common.service.cluster.ClusterInfo
 import com.tencent.bkrepo.common.service.cluster.ClusterProperties
-import com.tencent.bkrepo.common.service.cluster.RoleType
 import com.tencent.bkrepo.common.service.util.HttpContextHolder.getRequestOrNull
 import com.tencent.bkrepo.common.storage.core.StorageService
 import com.tencent.bkrepo.common.storage.credentials.StorageCredentials
@@ -252,7 +252,7 @@ class StorageManager(
         range: Range,
         storageKey: String?
     ): ArtifactInputStream? {
-        if (clusterProperties.role == RoleType.CENTER || !existInCenter(sha256, storageKey)) {
+        if (clusterProperties.role == ClusterNodeType.CENTER || !existInCenter(sha256, storageKey)) {
             return null
         }
         return loadFromCenter(sha256, range, storageKey)
